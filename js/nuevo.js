c11ff5cc1da5cf94278cd384ebab31c48af80689
Valentin Umbides Alvarez <valentinumbides8@gmail.com>
2023-01-10 20:11:06 -0300

    sin java

--- js/nuevo.js
+++ js/nuevo.js
@@ -1,31 +1,28 @@
 //setTimeout(() =>{
-    console.log("hola como va ?")
+    //console.log("hola como va ?")
 
-    var autor ="carlitos"
-    console.log(autor)
+    //var autor ="carlitos"
+    //console.log(autor)
     
-    let unColor = "con enconrdado"
-    if (unColor=="con enconrdado"){
-        console.log("la raqueta incluye encordado")
+    //let unTexto = "con enconrdado"
+   // if (unTexto=="con enconrdado"){
+       // console.log("la raqueta incluye encordado")
     
-    }else{
-        console.log("no incluye encordado")
-    }
+    //}else{
+   //     console.log("no incluye encordado")
+  //  }
     
     //let entrada = prompt ("ingresar nombre")
     //if( entrada == ""){
        // alert("el usuario ingreso"+ entrada)
-        //entrada = prompt ("ingresar apellido")
-        //console.log (typeof entrada)
-   // }
+       // entrada = prompt ("ingresar apellido")
+       // console.log (typeof entrada)
+    //}
 
 
-//},200)
-
-//setTimeout(() =>{
-    //for (let i = 1; i <=5; i++){
-        //let ingresarNombre = prompt("Ingresar tu Nombre para conocer nuestras sucursales")
-        //alert("turno n° "+i+" Nombre:" +ingresarNombre)
+//    for (let i = 1; i <=5; i++){
+ //       let ingresarNombre = prompt("Ingresar tu Nombre para conocer nuestras sucursales")
+   //     alert("turno n° "+i+" Nombre:" +ingresarNombre)
     //}
     
 //},200)
