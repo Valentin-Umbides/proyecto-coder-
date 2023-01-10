2f962cb39ec21137979bce39bfe093d0d3f884b8
Valentin Umbides Alvarez <valentinumbides8@gmail.com>
2023-01-10 12:13:37 -0300

    nuevo repo

---
+++ js/nuevo.js
@@ -0,0 +1,32 @@
+//setTimeout(() =>{
+    console.log("hola como va ?")
+
+    var autor ="carlitos"
+    console.log(autor)
+    
+    let unColor = "con enconrdado"
+    if (unColor=="con enconrdado"){
+        console.log("la raqueta incluye encordado")
+    
+    }else{
+        console.log("no incluye encordado")
+    }
+    
+    //let entrada = prompt ("ingresar nombre")
+    //if( entrada == ""){
+       // alert("el usuario ingreso"+ entrada)
+        //entrada = prompt ("ingresar apellido")
+        //console.log (typeof entrada)
+   // }
+
+
+//},200)
+
+//setTimeout(() =>{
+    //for (let i = 1; i <=5; i++){
+        //let ingresarNombre = prompt("Ingresar tu Nombre para conocer nuestras sucursales")
+        //alert("turno n° "+i+" Nombre:" +ingresarNombre)
+    //}
+    
+//},200)
+
