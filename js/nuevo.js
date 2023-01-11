26ccfdf14f289f522df3d122a197765138b3fa4a
Valentin Umbides Alvarez <valentinumbides8@gmail.com>
2023-01-10 21:33:55 -0300

    java

--- js/nuevo.js
+++ js/nuevo.js
@@ -1,29 +1,29 @@
-//setTimeout(() =>{
-    //console.log("hola como va ?")
+setTimeout(() =>{
+    console.log("hola como va ?")
 
-    //var autor ="carlitos"
-    //console.log(autor)
+    var autor ="carlitos"
+    console.log(autor)
     
-    //let unTexto = "con enconrdado"
-   // if (unTexto=="con enconrdado"){
-       // console.log("la raqueta incluye encordado")
+    let unTexto = "con enconrdado"
+    if (unTexto=="con enconrdado"){
+        console.log("la raqueta incluye encordado")
     
-    //}else{
-   //     console.log("no incluye encordado")
-  //  }
+    }else{
+        console.log("no incluye encordado")
+    }
     
-    //let entrada = prompt ("ingresar nombre")
-    //if( entrada == ""){
-       // alert("el usuario ingreso"+ entrada)
-       // entrada = prompt ("ingresar apellido")
-       // console.log (typeof entrada)
-    //}
+    let entrada = prompt ("ingresar nombre")
+    if( entrada == ""){
+        alert("el usuario ingreso"+ entrada)
+        entrada = prompt ("ingresar apellido")
+        console.log (typeof entrada)
+    }
 
 
-//    for (let i = 1; i <=5; i++){
- //       let ingresarNombre = prompt("Ingresar tu Nombre para conocer nuestras sucursales")
-   //     alert("turno n° "+i+" Nombre:" +ingresarNombre)
-    //}
+    for (let i = 1; i <=5; i++){
+    let ingresarNombre = prompt("Ingresar tu Nombre para conocer nuestras sucursales")
+        alert("turno n° "+i+" Nombre:" +ingresarNombre)
+    }
     
-//},200)
+},200)
 
